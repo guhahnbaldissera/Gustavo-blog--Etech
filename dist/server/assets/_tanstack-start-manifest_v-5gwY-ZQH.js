de5aca946422004b7d4c29154f4558d3cdfe8dd3
guhahnbaldissera <gu.hahnbaldissera@gmail.com>
2026-05-13 21:54:46 -0300

    adicionar past de build

---
+++ dist/server/assets/_tanstack-start-manifest_v-5gwY-ZQH.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/gusta/OneDrive/Desktop/Faculdade.Etech/Blog.gustavo/Gustavo-blog--Etech/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], assets: void 0, preloads: ["/assets/index-DVFzvmJE.js"] }, "/": { filePath: "C:/Users/gusta/OneDrive/Desktop/Faculdade.Etech/Blog.gustavo/Gustavo-blog--Etech/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-bYe12EK1.js"] } }, clientEntry: "/assets/index-DVFzvmJE.js" });
+export {
+  tsrStartManifest
+};
---
+++ dist/server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-BxZEtUq0.js";
+import "node:events";
+export {
+  w as default
+};
